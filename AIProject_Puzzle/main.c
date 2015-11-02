88ce93c78db15c6863b8564961f221497bcf0e73
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2015-11-02 21:23:34 -0200

    Call clock() function at the beginning and at the end of the algorithm execution

--- AIProject_Puzzle/main.c
+++ AIProject_Puzzle/main.c
@@ -9,10 +9,11 @@ int main() {
 	CreateHashTable();
 	CreateFirstState();
 	
-
+	clock_t start = clock();
 	do {
 		found = Pop();
 	} while (found !=0 && found!=1);
+	clock_t end = clock();
 
 	if (found == 0) {
 		printf("%s\n", "I am sorry, I could not find a reasonable result for you.");
@@ -20,6 +21,7 @@ int main() {
 	else if (found == 1) {
 		printf("\n\n%s\n\n", " >>>>>>> The actions must be followed FROM THE BOTTOM TO THE TOP!!! <<<<<<");
 		printf("%s\n", "The actions needed to find the result is stated above.");
+		printf("\n\nSpent time: %.4f\n\n", (float)(end - start) / CLOCKS_PER_SEC);
 	}
 
 	system("PAUSE");
